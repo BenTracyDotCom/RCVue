fd2847a166b5cbd04d55e36276d5800057c6a35a
Ben Tracy <b.rob.tracy@gmail.com>
2024-04-11 16:06:14 -0400

    map parts tiles to parts page

---
+++ resources/js/Components/PartTile.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <input type="checkbox" id="part" />
+    <label for="part">{{ label }}</label>
+    <h1>{{ title }}</h1>
+    <p>{{ description }}</p>
+    <div>{{ price }}</div>
+    <img :src="src" />
+  </div>
+</template>
+<script>
+//Define the kind of props this component wants
+  export default {
+    props: {
+      label: {required: true, type: String},
+      done: {default: false, type: Boolean},
+      title: {required: true, type: String},
+      type: {required: true, type: String},
+      description: {required: true, type: Text},
+      price: {required: true, type: Number},
+      image: {required: true, type: String}
+    },
+    data() {
+      return {
+        isDone: this.done,
+      }
+    }
+  }
+</script>
--- resources/js/Pages/Parts.vue
+++ resources/js/Pages/Parts.vue
@@ -1,9 +1,44 @@
 <script setup>
+import PartTile from '../Components/PartTile.vue';
 // Do all importing here
 </script>
 
 <template>
   <div>
-    Parts page!!
+    <h1>Parts page!!</h1>
+    <ul>
+      <li v-for="part in parts" :key="part.id">
+        <part-tile label="A Part Apart" :title="part.title" :type="part.type" :description="part.description"
+          :price="part.price" :image="part.image"></part-tile>
+      </li>
+    </ul>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+
+  },
+  data() {
+    return {
+      parts: [
+        {
+          title: 'ev800d',
+          type: 'digital',
+          description: 'the best pair of goggs you\'ll ever own',
+          price: 800.00,
+          image: 'https://via.placeholder.com/640x480.png/0066aa?text=ducimus',
+        },
+        {
+          title: 'QX7',
+          type: 'Transmitter',
+          description: 'Great beginner tx',
+          price: 79.99,
+          image: 'https://via.placeholder.com/640x480.png/0066aa?text=ducimus',
+        },
+      ],
+    };
+  },
+}
+</script>
